befa1f147aa646dd85832c4f5c0da55c94934e8a
Melo <102263606+Melo1337@users.noreply.github.com>
2024-05-20 22:45:00 -0300

    coloquei as cartas com dois valores iguais

--- src/script/script.js
+++ src/script/script.js
@@ -1,26 +1,39 @@
+let cards = document.querySelectorAll(".card");
 let numberCards = prompt("Escolha a quantidade de cartas, numeros pares de 4 a 14")
+let playCards = [];
+const backgroundGifs = [
+    'bobrossparrot',
+    'explodyparrot',
+    'fiestaparrot',
+    'metalparrot',
+    'revertitparrot',
+    'tripletsparrot',
+    'unicornparrot',
+];
+
+function backgroundGifIndice() {
+    const randomIndex = Math.floor(Math.random() * backgroundGifs.length);
+    indexGif = backgroundGifs[randomIndex];
+    return indexGif
+}
+
+
+function comparador() {
+    return Math.random() - 0.5;
+}
 
 function start() {
-
-    if (numberCards < 4 || numberCards > 14) {
-        for (let i = 0; numberCards % 2 != 0; i++) {
-            numberCards = prompt("Escolha a quantidade de cartas, numeros pares de 4 a 14")
-        }
-    } else {
-        let container = document.getElementsByClassName("tableCards");
-
-        let imgSrc = ["./src/img/bobrossparrot.gif", "./src/img/explodyparrot.gif", "./src/img/fiestaparrot.gif", "./src/img/metalparrot.gif", "./src/img/revertitparrot.gif", "./src/img/tripletsparrot.gif"];
-
+    if (numberCards % 2 == 0 && numberCards >= 4 && numberCards <= 14) {
+        numberCards = numberCards / 2
         for (let i = 0; i < numberCards; i++) {
-
-            let indiceAleatorio = Math.floor(Math.random() * imgSrc.length);
-            let srcAleatorio = imgSrc[indiceAleatorio]
-
-            let novaDiv = document.createElement("div");
-            novaDiv.classList.add("card");
-            container[0].appendChild(novaDiv).innerHTML = `<img src="${srcAleatorio}" />`;
+            backgroundGifIndice()
+            for (let i = 0; i < 2; i++) {
+                document.querySelector(".tableCards").innerHTML += `<div class="card new" onclick="turn(this)"><img src="/src/img/${indexGif}.gif"</div>`;
+            }
+            playCards.push(cards[i]);
         }
+        playCards = playCards.sort(comparador);
     }
-
 }
+
 start()
