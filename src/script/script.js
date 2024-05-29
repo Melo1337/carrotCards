3f72befd686add4bb464f5e71344b789e34d6efd
Melo <102263606+Melo1337@users.noreply.github.com>
2024-05-28 22:39:10 -0300

    cartas aleatorias

--- src/script/script.js
+++ src/script/script.js
@@ -1,6 +1,5 @@
-let cards = document.querySelectorAll(".card");
 let numberCards = prompt("Escolha a quantidade de cartas, numeros pares de 4 a 14")
-let playCards = [];
+let cards = document.querySelectorAll(".card");
 const backgroundGifs = [
     'bobrossparrot',
     'explodyparrot',
@@ -11,29 +10,73 @@ const backgroundGifs = [
     'unicornparrot',
 ];
 
-function backgroundGifIndice() {
-    const randomIndex = Math.floor(Math.random() * backgroundGifs.length);
-    indexGif = backgroundGifs[randomIndex];
-    return indexGif
+let baralho = [];
+
+function comparador() { 
+	return Math.random() - 0.5; 
 }
 
-
-function comparador() {
-    return Math.random() - 0.5;
-}
-
-function start() {
+function jogoInvalido() {
     if (numberCards % 2 == 0 && numberCards >= 4 && numberCards <= 14) {
-        numberCards = numberCards / 2
-        for (let i = 0; i < numberCards; i++) {
-            backgroundGifIndice()
-            for (let i = 0; i < 2; i++) {
-                document.querySelector(".tableCards").innerHTML += `<div class="card new" onclick="turn(this)"><img src="/src/img/${indexGif}.gif"</div>`;
-            }
-            playCards.push(cards[i]);
-        }
-        playCards = playCards.sort(comparador);
+        renderizarBaralho()
+    } else {
+        numberCards = prompt("erro tente novamento, numeros pares de 4 a 14")
     }
 }
 
-start()
+function renderizarBaralho() {
+    for (let i = 0; i < numberCards/2; i++) {
+        baralho.push(i)
+        baralho.push(i)
+    }
+    baralho.sort(comparador)
+
+    for (let i = 0; i < numberCards; i++) {
+        document.querySelector(".tableCards").innerHTML += `<div class="card new" onclick="turn(this)"><img src="/src/img/${backgroundGifs[baralho[i]]}.gif"</div>`;
+    }
+}
+
+jogoInvalido()
+
+console.log(baralho)
+// let playCards = [];
+// let cardsInGame = [];
+// const backgroundGifs = [
+//     'bobrossparrot',
+//     'explodyparrot',
+//     'fiestaparrot',
+//     'metalparrot',
+//     'revertitparrot',
+//     'tripletsparrot',
+//     'unicornparrot',
+// ];
+
+// function backgroundGifIndice() {
+//     const randomIndex = Math.floor(Math.random() * backgroundGifs.length);
+//     cardsInGame.push(randomIndex)
+//     nameGif = backgroundGifs[randomIndex];
+//     console.log(cardsInGame)
+//     return nameGif
+// }
+
+// function comparador() {
+//     return Math.random() - 0.5;
+// }
+
+// function start() {
+//     if (numberCards % 2 == 0 && numberCards >= 4 && numberCards <= 14) {
+//         numberCards = numberCards / 2
+//         for (let i = 0; i < numberCards; i++) {
+//             backgroundGifIndice()
+//             for (let i = 0; i < 2; i++) {
+//                 document.querySelector(".tableCards").innerHTML += `<div class="card new" onclick="turn(this)"><img src="/src/img/${nameGif}.gif"</div>`;
+//                 playCards.push(cards[i]);
+//             }
+           
+//         }
+        
+//     }
+//     playCards = playCards.sort(comparador);
+// }
+
+// start()
